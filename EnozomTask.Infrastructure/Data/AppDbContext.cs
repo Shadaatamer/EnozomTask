acc206a75fc29831c00cb72ec1f82fef3b0b8f3f
Shada Tamer <shadatamer.st@gmail.com>
2026-09-17 17:48:11 +0300

    added migrations

--- EnozomTask.Infrastructure/Data/AppDbContext.cs
+++ EnozomTask.Infrastructure/Data/AppDbContext.cs
@@ -125,98 +125,96 @@ new Trip { TripNumber = 6, TrainId = 3 }
        StopOrder = 3
    },
 
-    new TripStop
-    {
-        Id = 10,
-        TripId = 4,
-        StationId = 5,
-        Time = new TimeSpan(7, 0, 0),
-        StopOrder = 1
-    },
-    new TripStop
-    {
-        Id = 11,
-        TripId = 4,
-        StationId = 5,
-        Time = new TimeSpan(9, 0, 0),
-        StopOrder = 5
-    },
-
-
    new TripStop
    {
-       Id = 12,
-       TripId = 5,
+       Id = 10,
+       TripId = 4,
        StationId = 5,
-       Time = new TimeSpan(10, 30, 0),
+       Time = new TimeSpan(7, 0, 0),
        StopOrder = 1
    },
-   new TripStop
-   {
-       Id = 13,
-       TripId = 5,
-       StationId = 4,
-       Time = new TimeSpan(9, 30, 0),
-       StopOrder = 2
-   },
-   new TripStop
-   {
-       Id = 14,
-       TripId = 5,
-       StationId = 3,
-       Time = new TimeSpan(8, 30, 0),
-       StopOrder = 3
-   },
-   new TripStop
-   {
-       Id = 15,
-       TripId = 5,
-       StationId = 2,
-       Time = new TimeSpan(7, 30, 0),
-       StopOrder = 4
-   },
-   new TripStop
-   {
-       Id = 16,
-       TripId = 5,
-       StationId = 1,
-       Time = new TimeSpan(7, 30, 0),
-       StopOrder = 5
-   },
-         new TripStop
-         {
-             Id = 17,
-             TripId = 5,
-             StationId = 5,
-             Time = new TimeSpan(7, 30, 0),
-             StopOrder = 6
-         },
-   new TripStop
-   {
-       Id = 18,
-       TripId = 6,
-       StationId = 5,
-       Time = new TimeSpan(11, 30, 0),
-       StopOrder = 1
-   },
-   new TripStop
-   {
-       Id = 19,
+new TripStop
+{
+    Id = 11,
+    TripId = 4,
+    StationId = 5,
+    Time = new TimeSpan(9, 0, 0),
+    StopOrder = 5
+},
 
-       TripId = 6,
-       StationId = 3,
-       Time = new TimeSpan(10, 15, 0),
-       StopOrder = 2
-   },
-   new TripStop
-   {
-       Id = 20,
-       TripId = 6,
-       StationId = 5,
-       Time = new TimeSpan(9, 0, 0),
-       StopOrder = 3
-   }
+new TripStop
+{
+    Id = 12,
+    TripId = 5,
+    StationId = 5,
+    Time = new TimeSpan(10, 30, 0),
+    StopOrder = 1
+},
+new TripStop
+{
+    Id = 13,
+    TripId = 5,
+    StationId = 4,
+    Time = new TimeSpan(9, 30, 0),
+    StopOrder = 2
+},
+new TripStop
+{
+    Id = 14,
+    TripId = 5,
+    StationId = 3,
+    Time = new TimeSpan(8, 30, 0),
+    StopOrder = 3
+},
+new TripStop
+{
+    Id = 15,
+    TripId = 5,
+    StationId = 5,
+    Time = new TimeSpan(8, 0, 0),
+    StopOrder = 4
+},
+new TripStop
+{
+    Id = 16,
+    TripId = 5,
+    StationId = 1,
+    Time = new TimeSpan(7, 30, 0),
+    StopOrder = 5
+},
 
+new TripStop
+{
+    Id = 17,
+    TripId = 6,
+    StationId = 5,
+    Time = new TimeSpan(11, 30, 0),
+    StopOrder = 1
+},
+new TripStop
+{
+    Id = 18,
+    TripId = 6,
+    StationId = 3,
+    Time = new TimeSpan(10, 15, 0),
+    StopOrder = 2
+},
+new TripStop
+{
+    Id = 19,
+    TripId = 6,
+    StationId = 5,
+    Time = new TimeSpan(9, 30, 0),
+    StopOrder = 3
+},
+new TripStop
+{
+    Id = 20,
+    TripId = 6,
+    StationId = 1,
+    Time = new TimeSpan(9, 0, 0),
+    StopOrder = 4
+}
 );
     }
 }
---
+++ EnozomTask.Infrastructure/Migrations/20260917144422_UpdateTripStops.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnozomTask.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTripStops : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "StationId", "StopOrder" },
+                values: new object[] { 5, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "StationId", "Time" },
+                values: new object[] { 5, new TimeSpan(0, 8, 0, 0, 0) });
+
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "StationId", "Time" },
+                values: new object[] { 5, new TimeSpan(0, 9, 30, 0, 0) });
+
+            migrationBuilder.InsertData(
+                table: "TripStops",
+                columns: new[] { "Id", "StationId", "StopOrder", "Time", "TripId" },
+                values: new object[] { 20, 1, 4, new TimeSpan(0, 9, 0, 0, 0), 6 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "StationId", "StopOrder" },
+                values: new object[] { 1, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "StationId", "Time" },
+                values: new object[] { 2, new TimeSpan(0, 7, 30, 0, 0) });
+
+            migrationBuilder.UpdateData(
+                table: "TripStops",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "StationId", "Time" },
+                values: new object[] { 1, new TimeSpan(0, 9, 0, 0, 0) });
+        }
+    }
+}
